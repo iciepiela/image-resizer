67f6ead1fb0b242be8076b84f6468ef8da015e9b
ustankie <ustankiewicz@student.agh.edu.pl>
2024-11-29 18:44:22 +0100

    Initial commit

---
+++ image-resizer/src/main/java/pl/edu/agh/to/imageresizer/ImageResizerApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.imageresizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageResizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageResizerApplication.class, args);
+    }
+
+}
---
+++ image-resizer/src/test/java/pl/edu/agh/to/imageresizer/ImageResizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.imageresizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageResizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
