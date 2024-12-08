e7f69f1fe2eb871e110758896e9b6e96b3930cf7
iciepiela <ida.ciepiela0324@gmail.com>
2024-12-08 18:07:25 +0100

    table change

--- db-init.sql
+++ db-init.sql
@@ -7,5 +7,7 @@ CREATE TABLE IF NOT EXISTS original_image (
 -- Create table for ResizedImage
 CREATE TABLE IF NOT EXISTS resized_images (
     image_id SERIAL PRIMARY KEY,
-    path VARCHAR(255) NOT NULL
-);
+    image_key VARCHAR(255),
+    name VARCHAR(255),
+    base64 TEXT
+    );
